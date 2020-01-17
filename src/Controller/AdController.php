83ebb73b046b16bd910ac6c0e08d380e834828b6
Laurent Pichon <tech@lescachotieres.com>
2020-01-17 22:42:29 +0100

    Gestion des annonces, formulaire de création et d'édition

---
+++ src/Controller/AdController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Entity\Image;
+use App\Form\AdType;
+use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdController extends AbstractController
+{
+    /**
+     * Affichage de la liste des annonces
+     *
+     * @Route("/ads", name="ads_index")
+     * @param AdRepository $repo
+     * @param SessionInterface $session
+     * @return Response
+     */
+    public function index(AdRepository $repo)
+    {
+        $ads = $repo->findAll();
+
+        return $this->render('ad/index.html.twig', [
+            'ads' => $ads,
+        ]);
+    }
+
+
+    /**
+     * Permet de créer une annonce
+     *
+     * @Route("/ads/new", name="ads_create")
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function create(Request $request, EntityManagerInterface $manager)
+    {
+        $ad = new Ad();
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            foreach ($ad->getImages() as $image) {
+                $image->setAd($ad);
+
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'L\'annonce <strong> '.$ad->getTitle().' </strong> a bien été enregistrée !'
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug(),
+            ]);
+        }
+
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une annonce
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * @return Response
+     */
+    public function show(Ad $ad)
+    {
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad,
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'édition
+     *
+     * @Route("/ads/{slug}/edit", name="ads_edit")
+     * @param Request $request
+     * @param Ad $ad
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function edit(Request $request, Ad $ad, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            foreach ($ad->getImages() as $image) {
+                $image->setAd($ad);
+
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Les modifications de l\'annonce <strong> '.$ad->getTitle().' </strong> ont bien été enregistrées !'
+            );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug(),
+            ]);
+        }
+
+        return $this->render('ad/edit.html.twig', [
+            'form' => $form->createView(),
+            'ad'   => $ad,
+        ]);
+    }
+}
